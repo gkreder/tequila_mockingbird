e4f6ff9fd32aede03a9fba3e23b688e22ab81b6b
gabe-lisa-kevin-rulin-beatrice <tqm@derisilab105.ucsf.edu>
2016-10-09 14:12:39 -0700

    added TA code

--- image_processing/gabe.py
+++ image_processing/gabe.py
@@ -1,8 +1,25 @@
-import os
-import sys
+from skimage.morphology import watershed, disk
+from skimage.filters import sobel
+from skimage import exposure
+from skimage import transform as tf
+from skimage.segmentation import slic, join_segmentations
+from skimage import data, img_as_float
+from scipy import fftpack
+from skimage.filters.rank import median
+from scipy import ndimage as ndi
 
-base_dir = os.path.normpath(os.path.dirname(os.getcwd()))
-# base_dir = 
+def segment_image(photo_matrix, background_threshold, foreground_threshold):
+    edges = sobel(photo_matrix)
+    markers = np.zeros_like(photo_matrix)
+    foreground, background = 1, 2
+    markers[photo_matrix < background_threshold] = background
+    markers[photo_matrix > foreground_threshold] = foreground
+    ws = watershed(edges, markers)
+    segmentation_matrix = ndi.label(ws == foreground)[0]
+    return segmentation_matrix
 
-print base_dir
+
+
+
+
 
